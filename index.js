d1bb6a44d2ac58d66486b3bf86e5cbcfcb1df952
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-22 14:27:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let input = document.getElementById('button');
+//Bind an event listener.
 function addingEventListener() {
+//Pass a reference to the function     
+  input.addEventListener('click', function(){
+      });
 }
